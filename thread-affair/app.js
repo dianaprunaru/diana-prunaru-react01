fd1cda46974f3ea340029f890fced7c2fbd044fb
dianaprunaru <mosoianudiana@gmail.com>
2022-09-05 22:06:23 +0300

    newsletter component

---
+++ thread-affair/app.js
@@ -0,0 +1,83 @@
+class NewsletterForm extends React.Component {
+  state = {
+    email: '',
+    formMessage: '',
+    busy: false,
+    successMessage: '',
+  };
+
+  validateEmail(email) {
+    const re =
+      /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(email);
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const email = this.state.email;
+
+    if (!this.validateEmail(email)) {
+      this.setState({
+        formMessage: 'Please use a valid email',
+      });
+      return;
+    }
+
+    this.setState({
+      busy: true,
+      formMessage: '',
+    });
+
+    setTimeout(() => {
+      this.setState({
+        busy: false,
+        email: '',
+        successMessage: `Emailul ${this.state.email} a fost inscris.`,
+      });
+    }, 3000);
+  };
+
+  // controlled submit/input
+  onInputChange = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+
+  // render runs everytime state changes
+  render() {
+    const isSubmitted = this.state.successMessage.trim().length > 0;
+
+    if (isSubmitted) {
+      return <div className="container">{this.state.successMessage}</div>;
+    }
+    // render must return JSX
+    return (
+      <form className="form-newsletter container" onSubmit={this.onSubmit}>
+        <label htmlFor="field-newsletter">
+          Subscribe to our <span>newsletter</span>
+        </label>
+
+        <input
+          type="text"
+          name="field-newsletter"
+          id="field-newsletter"
+          placeholder="boo"
+          onChange={this.onInputChange}
+          value={this.state.email}
+        ></input>
+
+        <button title="Subscribe" type="submit" disabled={this.state.busy}>
+          {this.state.busy ? '...loading' : 'Subscribe'}
+        </button>
+
+        <div className="form-message">{this.state.formMessage}</div>
+      </form>
+    );
+  }
+}
+
+const newsletterContainer = document.querySelector('.home-newsletter');
+// React recipe?
+ReactDOM.render(<NewsletterForm></NewsletterForm>, newsletterContainer);
