8c540c91f67a612ecd6a69845645f1c4b4f0bb05
dianaprunaru <mosoianudiana@gmail.com>
2022-09-07 17:37:46 +0300

    Tema 5 Septembrie

---
+++ monochrome/app.js
@@ -0,0 +1,81 @@
+class NewsletterForm extends React.Component {
+  state = {
+    email: '',
+    formMessage: '',
+    busy: false,
+    successMessage: '',
+  };
+
+  validateEmail(email) {
+    const re =
+      /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(email);
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const email = this.state.email;
+
+    if (!this.validateEmail(email)) {
+      this.setState({
+        formMessage: 'Please use a valid email',
+      });
+
+      return;
+    }
+
+    this.setState({
+      busy: true,
+      formMessage: '',
+    });
+
+    setTimeout(() => {
+      this.setState({
+        busy: false,
+        email: '',
+        successMessage: `Your email ${this.state.email} has been submitted.`,
+      });
+    }, 3000);
+  };
+
+  onInputChange = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+
+  render() {
+    const isSubmitted = this.state.successMessage.trim().length > 0;
+
+    if (isSubmitted) {
+      return <div className="container">{this.state.successMessage}</div>;
+    }
+
+    return (
+      <form className="form-newsletter container" onSubmit={this.onSubmit}>
+        <label htmlFor="email-newsletter">sign up for our newsletter</label>
+
+        <input
+          type="text"
+          name="email-newsletter"
+          id="email-newsletter"
+          placeholder="enter email"
+          onChange={this.onInputChange}
+          value={this.state.email}
+        ></input>
+
+        <button title="Submit" type="submit" disabled={this.state.busy}>
+          {this.state.busy ? '...loading' : 'Submit'}
+        </button>
+
+        <div className="form-message">{this.state.formMessage}</div>
+      </form>
+    );
+  }
+}
+
+const newsletterContainer = document.querySelector(
+  '.footer-sign-up-newsletter',
+);
+ReactDOM.render(<NewsletterForm></NewsletterForm>, newsletterContainer);
