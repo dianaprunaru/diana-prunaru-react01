72155425d95cc14eade2d55455b9b2bc22ad4815
dianaprunaru <mosoianudiana@gmail.com>
2022-09-08 10:52:05 +0300

    a2c and wishlist

--- thread-affair/app.js
+++ thread-affair/app.js
@@ -1,3 +1,6 @@
+const ADD_TO_CART_EVENT = 'cart/productAdded';
+const REMOVE_FROM_CART_EVENT = 'cart/productRemoved';
+
 class NewsletterForm extends React.Component {
   state = {
     email: '',
@@ -8,11 +11,12 @@ class NewsletterForm extends React.Component {
 
   validateEmail(email) {
     const re =
-      /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     return re.test(email);
   }
 
+  // event handlers need "this"
   onSubmit = (event) => {
     event.preventDefault();
     const email = this.state.email;
@@ -21,6 +25,7 @@ class NewsletterForm extends React.Component {
       this.setState({
         formMessage: 'Please use a valid email',
       });
+
       return;
     }
 
@@ -38,7 +43,7 @@ class NewsletterForm extends React.Component {
     }, 3000);
   };
 
-  // controlled submit/input
+  // controlled component/input
   onInputChange = (event) => {
     this.setState({
       email: event.target.value,
@@ -52,7 +57,8 @@ class NewsletterForm extends React.Component {
     if (isSubmitted) {
       return <div className="container">{this.state.successMessage}</div>;
     }
-    // render must return JSX
+
+    // render must -RETURN- JSX
     return (
       <form className="form-newsletter container" onSubmit={this.onSubmit}>
         <label htmlFor="field-newsletter">
@@ -63,13 +69,13 @@ class NewsletterForm extends React.Component {
           type="text"
           name="field-newsletter"
           id="field-newsletter"
-          placeholder="boo"
+          placeholder="vrem sa iesim la pauza =))"
           onChange={this.onInputChange}
           value={this.state.email}
         ></input>
 
-        <button title="Subscribe" type="submit" disabled={this.state.busy}>
-          {this.state.busy ? '...loading' : 'Subscribe'}
+        <button title="Subcribe" type="submit" disabled={this.state.busy}>
+          {this.state.busy ? '...loading' : 'Submit'}
         </button>
 
         <div className="form-message">{this.state.formMessage}</div>
@@ -81,3 +87,185 @@ class NewsletterForm extends React.Component {
 const newsletterContainer = document.querySelector('.home-newsletter');
 // React recipe?
 ReactDOM.render(<NewsletterForm></NewsletterForm>, newsletterContainer);
+
+class AddToCartButton extends React.Component {
+  state = {
+    added: false,
+    busy: false,
+  };
+
+  onClick = () => {
+    // NO
+    // this.state.added = !this.state.added
+
+    this.setState({
+      busy: true,
+    });
+
+    setTimeout(() => {
+      const eventName = this.state.added
+        ? REMOVE_FROM_CART_EVENT
+        : ADD_TO_CART_EVENT;
+
+      dispatchEvent(
+        new CustomEvent(eventName, {
+          detail: {
+            productId: this.props.productId,
+          },
+        }),
+      );
+
+      this.setState({
+        added: !this.state.added,
+        busy: false,
+      });
+    }, 2000);
+  };
+
+  // all components require a render
+  render() {
+    // render must return jsx
+    return (
+      <button
+        className={`product-control ${this.state.added ? 'active' : ''}`}
+        onClick={this.onClick}
+        type="button"
+        title={this.state.added === true ? 'Remove from Cart' : 'Add to Cart'}
+        disabled={this.state.busy}
+      >
+        {this.state.added === true
+          ? `PID: ${this.props.productId} in cart`
+          : 'Add to Cart'}
+        {this.state.busy ? <i className="fas fa-spinner"></i> : ''}
+      </button>
+    );
+  }
+}
+
+// function react component
+const AddToWishlistButton = ({ productId }) => {
+  const state = React.useState({
+    added: false,
+    busy: false,
+  });
+  const actualState = state[0];
+  const setState = state[1];
+
+  const onClick = () => {
+    setState({
+      added: actualState.added,
+      busy: true,
+    });
+
+    setTimeout(() => {
+      // dispatch event
+
+      setState({
+        added: !actualState.added,
+        busy: false,
+      });
+    }, 500);
+  };
+
+  return (
+    <button
+      className={`product-control ${actualState.added ? 'active' : ''}`}
+      title={actualState.added ? 'Remove from Wishlist' : 'Add to Wishlist'}
+      type="button"
+      onClick={onClick}
+    >
+      {actualState.added === true
+        ? `PID: ${productId} in wishlist`
+        : 'Add to Wishlist'}
+      {actualState.busy ? <i className="fas fa-spinner"></i> : ''}
+    </button>
+  );
+};
+
+class ProductControls extends React.Component {
+  render() {
+    return [
+      <AddToCartButton
+        key="cart"
+        productId={this.props.productId}
+      ></AddToCartButton>,
+      <AddToWishlistButton
+        key="wl"
+        productId={this.props.productId}
+      ></AddToWishlistButton>,
+    ];
+  }
+}
+
+const productTileControls = document.querySelectorAll('.product-tile-controls');
+productTileControls.forEach((productTileControl, index) => {
+  ReactDOM.render(
+    <ProductControls productId={index}></ProductControls>,
+    productTileControl,
+  );
+});
+
+class HeaderCounters extends React.Component {
+  state = {
+    cartItemsCount: 0,
+    cartItems: [],
+  };
+
+  componentDidMount() {
+    addEventListener(ADD_TO_CART_EVENT, (event) => {
+      const productId = event.detail.productId;
+      // slice will clone the array
+      const cartItems = this.state.cartItems.slice();
+      cartItems.push(productId);
+
+      this.setState({
+        cartItemsCount: cartItems.length,
+        cartItems,
+      });
+    });
+
+    addEventListener(REMOVE_FROM_CART_EVENT, (event) => {
+      const productId = event.detail.productId;
+      const cartItems = this.state.cartItems.filter((cartItem) => {
+        return productId !== cartItem;
+      });
+
+      this.setState({
+        cartItemsCount: cartItems.length,
+        cartItems,
+      });
+    });
+  }
+
+  showProducts = () => {
+    let message = '';
+
+    if (this.state.cartItems.length <= 0) {
+      message = 'There are no products in your cart';
+    } else {
+      message = `These are the pids in your cart: ${this.state.cartItems}.`;
+    }
+
+    alert(message);
+  };
+
+  render() {
+    return (
+      <>
+        <div className="header-counter" onClick={this.showProducts}>
+          <span className="qty">{this.state.cartItemsCount}</span>
+          <i className="fas fa-heart icon"></i>
+        </div>
+
+        <div className="header-counter" onClick={this.showProducts}>
+          <span className="qty">{this.state.cartItemsCount}</span>
+          <i className="fas fa-shopping-cart icon"></i>
+        </div>
+      </>
+    );
+  }
+}
+
+const headerCounters = document.querySelector('.header-counters');
+// mount react the good way
+ReactDOM.createRoot(headerCounters).render(<HeaderCounters></HeaderCounters>);
