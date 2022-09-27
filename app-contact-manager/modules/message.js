d3d75731fee82f97b8416ffbee45a342ff80f1cd
dianaprunaru <mosoianudiana@gmail.com>
2022-09-27 17:10:35 +0300

    Tema 12 septembrie

--- app-contact-manager/modules/message.js
+++ app-contact-manager/modules/message.js
@@ -1,9 +1,24 @@
+import { clearMessages } from './notificationBar.js';
+
 export default (message = '', type = 'primary') => {
   const messageContainer = document.createElement('div');
+
   messageContainer.classList.add('alert', `alert-${type}`);
 
   // FYI exista un textContent
   messageContainer.textContent = message;
+
+  messageContainer.innerHTML = `
+  <div>${message}</div>
+  <button type="button"
+      title="Close"
+      id="close-button"
+      class="btn btn-close close-button"
+    ></button>
+  `;
+
+  const closeBtn = document.getElementById('close-button');
+  closeBtn.addEventListener('click', clearMessages);
 
   return messageContainer;
 };
